5c0f8d265b1eb24b8da8ff17e395e4354ac96ed1
SimoneSerraCassano <55654332+SimoneSerraCassano@users.noreply.github.com>
2021-03-25 22:49:38 +0100

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,23 @@
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validate() {
+  const $result = $("#result");
+  const email = $("#email").val();
+  $result.text("");
+  const $error = $("#error");
+
+  if (validateEmail(email)) {
+    $result.text(email + " is valid :)");
+    $result.css("color", "green");
+  } else {
+    $result.text(email + " is not valid :(");
+    $result.css("color", "red");
+    $error.show();
+  }
+  return false;
+}
+
+$("#validate").on("click", validate);
